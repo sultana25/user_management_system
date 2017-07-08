e1b43fb86367940c1fd2de31e49920e1f67dfdd0
sultana <ssazeda@gmail.com>
2017-07-08 22:49:29 +0600

    modified

--- resources/views/admin/comments/show.blade.php
+++ resources/views/admin/comments/show.blade.php
@@ -24,7 +24,7 @@
                 <td>{{$comment->author}}</td>
                 <td>{{$comment->email}}</td>
                 <td>{{str_limit($comment->body, 40)}}</td>
-                <td><a href="{{route('home.post',$comment->post->id)}}">View Post</a></td>
+                <td><a href="{{route('home.post',$comment->post->slug)}}">View Post</a></td>
                 <td>
                    @if($comment->is_active)
                     {!!Form::open(['method'=>'PATCH','action'=>['PostCommentsController@update',$comment->id]])!!}
